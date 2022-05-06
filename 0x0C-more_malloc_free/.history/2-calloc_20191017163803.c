09c3be3cf1fe9efd61848b859a8162c3dc500ea9
Muhammed <kiotserver@gmail.com>
2022-05-06 20:52:31 +0300

    Done

---
+++ 0x0C-more_malloc_free/.history/2-calloc_20191017163803.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* string_nconcat - prints concatenate string;
+* @nmemb: input string.
+* @size: input string.
+* Return: Nothing.
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
